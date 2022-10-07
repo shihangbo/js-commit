7b68c0eefffee990e5e863bd3b3b4dae5afb307a
shihangbo <shihangbo@126.com>
2022-10-07 11:18:35 +0800

    add index

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+#!/usr/bin/env node
+"use strict";
+const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
+bootstrap({
+  cliPath: path.join(__dirname, '../../node_modules/commitizen'),
+  // this is new
+  config: {
+    "path": "cz-conventional-changelog"
+  }
+});
