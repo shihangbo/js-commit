67bc4b7ceef36624c50f88c77a670afcf22c16d5
shihangbo <shihangbo@126.com>
2022-10-07 19:39:24 +0800

    增加代码校验

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 const path = require('path');
-const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
-bootstrap({
-  cliPath: path.join(__dirname, '../../commitizen'),
-  // this is new
-  config: {
-    "path": "cz-conventional-changelog"
-  }
-});
+const fs = require('fs');
+const mainPath = path.join(process.cwd(), 'src/index.js');
+const mainMsg = fs.readFileSync(mainPath,"utf-8");
+// console.log(mainMsg, 111);
+const isSensorsInit = mainMsg.indexOf('sensors.init') > -1
+if (isSensorsInit) {
+  process.exit(0);
+} else {
+  throw new Error("请初始化 imou-sensors 埋点信息！");
+}
